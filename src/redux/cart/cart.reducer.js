f14d58ff24752d5777a145a1db0dd3233c0e850c
manoj <manojkarthikeya@gmail.com>
2021-09-27 18:19:45 +0530

    added cart selector

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import types from "./cart.types";
-
+import { addItemToCart } from "./cart.utils";
 const INITIAL_STATE = {
 	hidden: true,
 	cartItems: []
@@ -15,7 +15,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 		case types.ADD_ITEM:
 			return{
 				...state,
-				cartItems: [...state.cartItems, action.payload]
+				cartItems: addItemToCart(state.cartItems, action.payload)
 			};
 		default:
 			return state;
